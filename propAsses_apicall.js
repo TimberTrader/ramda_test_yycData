258d496abe4bde01ad72a62864fed562fe085b60
TimberTrader <tp.bowes@gmail.com>
2019-06-14 15:42:20 -0600

    encoutered buffer limits in attempt to dwnld entire dataset, broke into years ~500,000 records, 250 mb - success for 18, 19 years

--- propAsses_apicall.js
+++ propAsses_apicall.js
@@ -7,7 +7,7 @@ const resourceIDMap = {
 
 function apiCall(resourceID, callBack) {
   let options = {
-    url: `https://data.calgary.ca/resource/${resourceIDMap[resourceID]}.json?$Limit=500000&assessment_class=RE`,
+    url: `https://data.calgary.ca/resource/${resourceIDMap[resourceID]}.json?$Limit=750000&assessment_class=RE&roll_year=2018`,
     headers: {
       'User-Agent': 'request',
       'X-App-Token': 'TuumEdQ9KIehmtGnn2QjJoes7'
@@ -18,7 +18,7 @@ function apiCall(resourceID, callBack) {
     console.log('statusCode:', response && response.statusCode);
     let dataObj = JSON.parse(body);
     console.log(dataObj.length);
-    fs.writeJson('./test.json', {dataObj}, err => {
+    fs.writeJson('./data2018.json', {dataObj}, err => {
       if (err) return console.error(err)
     
       console.log('success!')
